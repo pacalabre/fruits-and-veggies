3c87872f7c706ac9d7a9b22cf0030b0023fba175
Paul <pacalabre@gmail.com>
2016-05-23 15:16:41 -0700

    created main Angular starter code

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,6 +1,29 @@
 /* setup your angular app here */
-
+var app = angular.module('FruitVeggiesApp', []);
 //debug stuff to show the app is loading and fruit / veggies are available
+app.controller('FruitVeggiesCtrl',['$scope',function($scope) {
+  $scope.fruit = [
+    'Apple',
+    'Apricot',
+    'Banana'
+    ];
+
+  $scope.veggies = [
+    'Artichoke',
+    'Arugula',
+    'Asparagus'
+    ];
+
+  $scope.fruitAndVeg = [
+    'Artichoke',
+    'Arugula',
+    'Asparagus',
+    'Apple',
+    'Apricot',
+    'Banana'
+  ]
+
+}])
 console.log('App Started');
 console.log('Fruit count', fruit.length);
 console.log('Veggie count', vegetables.length);
